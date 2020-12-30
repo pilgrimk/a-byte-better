8b1405bbd652372dd1a45b2cfe0c093a36ef5090
Kevin Pilgrim <pilgrimka1@yahoo.com>
2020-12-30 15:00:25 -0700

    Worked on bg-image

--- src/Components/BottomCards.js
+++ src/Components/BottomCards.js
@@ -12,18 +12,13 @@ function BottomCards() {
                     <Col>
                         <Card style={{ width: "20rem" }}>
                             <Card.Body>
-                                <Card.Title>Card 1 Title</Card.Title>
-                                <Card.Subtitle className="mb-2 text-muted">
-                                    Card Subtitle
-                                </Card.Subtitle>
-                                <Card.Text>
-                                    Some quick example text to build on the card title and make up
-                                    the bulk of the card's content.
+                                <Card.Title style={{textAlign:"center"}}>About Us</Card.Title>
+                                <Card.Text style={{textAlign:"left"}}>
+                                    We are a small computer lab specializing in networking and servers for small to 
+                                    medium sized businesses and residential customers.<br></br>
                                 </Card.Text>
-                                <Card.Link href="#">Card Link</Card.Link>
-                                <Card.Link href="#">Another Link</Card.Link>
                             </Card.Body>
-                        </Card>;
+                        </Card>
                     </Col>
                     <Col>
                         <Card style={{ width: "20rem" }}>
@@ -39,23 +34,23 @@ function BottomCards() {
                                 <Card.Link href="#">Card Link</Card.Link>
                                 <Card.Link href="#">Another Link</Card.Link>
                             </Card.Body>
-                        </Card>;
+                        </Card>
                     </Col>
                     <Col>
                         <Card style={{ width: "20rem" }}>
-                            <Card.Body>
-                                <Card.Title>Card 3 Title</Card.Title>
-                                <Card.Subtitle className="mb-2 text-muted">
-                                    Card Subtitle
-                                </Card.Subtitle>
-                                <Card.Text>
-                                    Some quick example text to build on the card title and make up
-                                    the bulk of the card's content.
+                        <Card.Body>
+                                <Card.Title style={{textAlign:"center"}}>Contact Addresses</Card.Title>
+                                <Card.Text style={{textAlign:"left"}}>
+                                    Address: 140 W 9000 S, Ste 4<br></br>
+                                             Sandy, Utah 84070<br></br>
+                                    Mobile: (801) 865-6571<br></br>
+                                    Email: bob@byte-better.com<br></br>
+                                    <br></br>
+                                    Social Media:
                                 </Card.Text>
-                                <Card.Link href="#">Card Link</Card.Link>
-                                <Card.Link href="#">Another Link</Card.Link>
+                                <Card.Link href="https://www.facebook.com/AByteBetterComputers">Facebook</Card.Link>
                             </Card.Body>
-                        </Card>;
+                        </Card>
                     </Col>
                 </Row>
             </Container>
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,8 +3,6 @@ import React from 'react';
 function Home(){
     return(
         <div className="myContent">
-          <h1>Home Page</h1>
-          <img src="UnderConstruction.png" alt="under construction" />
         </div>
       )
 }
